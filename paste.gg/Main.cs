cc5ea870df18739ebeac2639d3ebea8deef7bd41
Joshua Ebert <selyx666@gmail.com>
2021-04-13 13:51:26 +0200

    Added the ability to open up paste.gg quickly

--- paste.gg/Main.cs
+++ paste.gg/Main.cs
@@ -1,6 +1,38 @@
-﻿namespace paste.gg
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using paste.gg.Properties;
+using Wox.Plugin;
+
+namespace paste.gg
 {
-    public class Main
+    public class Main : IPlugin
     {
+        public List<Result> Query(Query query)
+        {
+            //TODO: add the ability to add pastes directly using the paste.gg api.
+            var possibleResults = new List<Result>();
+
+            var result = new Result
+            {
+                Title = "paste.gg",
+                SubTitle = "Add a new paste",
+                IcoPath = Resources.icon,
+                Action = context =>
+                {
+                    Process.Start("https://paste.gg");
+                    return true;
+                }
+            };
+
+
+            possibleResults.Add(result);
+
+            return possibleResults;
+        }
+
+        public void Init(PluginInitContext context)
+        {
+        }
     }
 }
